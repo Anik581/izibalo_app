ed36b1e66607a158091d3e47a33de48e307715b0
anik581 <581anik@gmail.com>
2014-10-05 22:35:47 +0200

    names of the months and days in calendar

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -8,15 +8,16 @@ jQuery(function(){
 	$(document).ready(function() {
 		var visible_calendar_width = $("#visible_calendar").width();
 		var hidden_calendar_width = $("#hidden_calendar").width();
+		var target_margin = (parseInt($(".day").css("margin-top")))
 		var space_on_the_right_of_trget = parseInt($(".day").css("width"));
   	var space_without_calendar = ($(".container").offset().left) + parseInt($(".container").css("padding-left"));
-  	var move = ($( ".target" ).offset().left + (2 * space_on_the_right_of_trget) - space_without_calendar) - parseInt($("#visible_calendar").css("padding-left"));
+  	var move = ($( ".target" ).offset().left + (2 * space_on_the_right_of_trget)+(2 * target_margin) - space_without_calendar) - parseInt($("#visible_calendar").css("margin-left"));
  		var validate_move = 0
 		if (move - visible_calendar_width > 0) {
 		  if ((move - visible_calendar_width) < (hidden_calendar_width - visible_calendar_width)) {
 		    validate_move = move - visible_calendar_width;
 		  } else {
-		    validate_move = (move - space_on_the_right_of_trget) - visible_calendar_width;
+		    validate_move = ((move - space_on_the_right_of_trget) - (2 * target_margin)) - visible_calendar_width;
 		  }
 		}
 		$('#visible_calendar').niceScroll('#hidden_calendar',{
@@ -30,3 +31,4 @@ jQuery(function(){
 	});
 
 });
+
