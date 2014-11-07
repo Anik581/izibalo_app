86a3c66238f82cf2a13eb54898d5c32da3527530
anik581 <581anik@gmail.com>
2014-11-07 21:27:42 +0100

    default scope of days and tasks for postgres / heroku - next attempt

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,8 +1,4 @@
 class Day < ActiveRecord::Base
 	belongs_to :task
-<<<<<<< HEAD
 	default_scope { order('date ASC') } 
-=======
-	default_scope order('date ASC')
->>>>>>> a7a81149aff0eaa4409c7672f938184552e46b1d
 end
