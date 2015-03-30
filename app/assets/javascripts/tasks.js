116763eee4a757c6cc90faf466a34b4ee29d4f37
anik581 <581anik@gmail.com>
2015-03-31 00:18:18 +0200

    improved js

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,118 +1,134 @@
+$(function() {
 
-//.............................................................new
+  InitTasks = function() {
 
-jQuery(function(){
-  $("#task_max_work_time").change(function () {                    
-		var newValue = $('#task_max_work_time').val();
-		$("#display").html("Max hours for day - " + newValue);
-	})
-});
+    $(document).ready(function() {
+      DisplayMaxWorkTime();
+      if ( $("#hidden_calendar").length > 0 ) {
+        CalendarAutoScroll();
+      };
+      if ( $("#area_chart_overall").length > 0 ) {
+        InitAreaChartOverall();
+        $(window).resize(function(){ InitAreaChartOverall() });
+      };
+      if ( $("#pie_chart_task_progress").length > 0 ) {
+        InitPieChartProgress();
+        $(window).resize(function(){ InitPieChartProgress() });
+      };
+      if ( $("#pie_chart_time_details").length > 0 ) {
+        InitPieChartTimeDetails();
+        $(window).resize(function(){ InitPieChartTimeDetails() });
+      };
+    });
 
-//.............................................................show
+  };
 
-jQuery(function(){
-	$(document).ready(function() {
+  DisplayMaxWorkTime = function() {
+   $("#task_max_work_time").on("change", function () {
+      var hour = $('#task_max_work_time').val();
+      $("#display").html("Max hours for day - " + hour);
+    }); 
+  };
+
+  CalendarAutoScroll = function() {
     var hidden_calendar = $("#hidden_calendar");
     var visible_calendar = $("#visible_calendar");
     var target = hidden_calendar.find(".target");
     if ( target.length == 0 ) { target = $(hidden_calendar).find(".day").last() };
-		var visible_calendar_width = visible_calendar.width();
-		var hidden_calendar_width = hidden_calendar.width();
-		var target_margin = (parseInt($(".day").css("margin-top")));
-		var space_on_the_right_of_target = parseInt($(".day").css("width"));
-  	var space_without_calendar = ($(".container").offset().left) + parseInt($(".container").css("padding-left"));
-  	var move = ($(target).offset().left + (2 * space_on_the_right_of_target)+(2 * target_margin) - space_without_calendar) - parseInt($("#visible_calendar").css("margin-left"));
- 		var validate_move = 0
-		if (move - visible_calendar_width > 0) {
-		  if ((move - visible_calendar_width) < (hidden_calendar_width - visible_calendar_width)) {
-		    validate_move = move - visible_calendar_width;
-		  } else {
-		    validate_move = ((move - space_on_the_right_of_target) - (2 * target_margin)) - visible_calendar_width;
-		  }
-		}
-		$(visible_calendar).niceScroll(hidden_calendar,{
-			cursorborder: false,
-			background: "#999",
-	    cursorcolor:"white",
-	    cursorwidth:"14px",
-	    cursorborderradius: "5px",
-			autohidemode: false
-		}).doScrollLeft(	validate_move	 );
-	});
+    var visible_calendar_width = visible_calendar.width();
+    var hidden_calendar_width = hidden_calendar.width();
+    var target_margin = (parseInt($(".day").css("margin-top")));
+    var space_on_the_right_of_target = parseInt($(".day").css("width"));
+    var space_without_calendar = ($(".container").offset().left) + parseInt($(".container").css("padding-left"));
+    var move = ($(target).offset().left + (2 * space_on_the_right_of_target)+(2 * target_margin) - space_without_calendar) - parseInt($("#visible_calendar").css("margin-left"));
+    var validate_move = 0
+    if (move - visible_calendar_width > 0) {
+      if ((move - visible_calendar_width) < (hidden_calendar_width - visible_calendar_width)) {
+        validate_move = move - visible_calendar_width;
+      } else {
+        validate_move = ((move - space_on_the_right_of_target) - (2 * target_margin)) - visible_calendar_width;
+      }
+    }
+    $(visible_calendar).niceScroll(hidden_calendar,{
+      cursorborder: false,
+      background: "#999",
+      cursorcolor:"white",
+      cursorwidth:"14px",
+      cursorborderradius: "5px",
+      autohidemode: false
+    }).doScrollLeft(  validate_move  );
+  };
+
+  InitAreaChartOverall = function() {
+    google.load('visualization', '1', {packages:["corechart"], callback: AreaChartOverall});
+  };
+
+  AreaChartOverall = function() {
+    var area_chart_overall = $("#area_chart_overall");
+    var array = area_chart_overall.data("active-days");
+    var title = area_chart_overall.data("area-chart-title");
+    var hours_vaxis = area_chart_overall.data("hours-vaxis");
+    var data = google.visualization.arrayToDataTable(array);
+    var options = {
+      title: title,
+      titleTextStyle: {color: '#B2B2B2'},
+      hAxis: {textStyle: {color: '#B2B2B2'}},
+      vAxis: {textStyle: {color: '#B2B2B2'},
+              minValue: 0,
+              ticks: hours_vaxis },
+      backgroundColor: '#2e3137',
+      legend: {position: 'none'},
+      chartArea: {backgroundColor: '#43464B'},
+      colors:['#00CC00']
+    };
+    var chart = new google.visualization.AreaChart( area_chart_overall[0] );
+    chart.draw(data, options);
+  };
+
+  InitPieChartProgress = function() {
+    google.load('visualization', '1', {packages:["corechart"], callback: PieChartProgress});
+  };
+
+  PieChartProgress = function() {
+    var array = $("#pie_chart_task_progress").data("task-progress");
+    var title = $("#pie_chart_task_progress").data("task-title");
+    var data = google.visualization.arrayToDataTable(array);
+    var options = {
+      title: title,
+      titleTextStyle: {color: '#B2B2B2'},
+      backgroundColor: '#2e3137',
+      legend: {textStyle: {color: '#B2B2B2'}},
+      pieSliceBorderColor: '#B2B2B2',
+      slices: {0: {color: '#008A00'}, 1: {color: '#43464B'}},
+      chartArea: {top: 70},
+      height: 350,
+    };
+    var chart = new google.visualization.PieChart( $("#pie_chart_task_progress")[0] );
+    chart.draw(data, options);
+  };
+
+  InitPieChartTimeDetails = function() {
+    google.load('visualization', '1', {packages:["corechart"], callback: PieChartTimeDetails});
+  };
+
+  PieChartTimeDetails = function() {
+    var array = $("#pie_chart_time_details").data("time-details");
+    var slices_colors = $("#pie_chart_time_details").data("slices-colors");
+    var data = google.visualization.arrayToDataTable(array);
+    var options = {
+      title: 'Activity time in details',
+      titleTextStyle: {color: '#B2B2B2'},
+      backgroundColor: '#2e3137',
+      legend: {textStyle: {color: '#B2B2B2'}},
+      pieSliceBorderColor: '#B2B2B2',
+      slices: slices_colors,
+      chartArea: {top: 70},
+      height: 350,
+    };
+    var chart = new google.visualization.PieChart( $("#pie_chart_time_details")[0] );
+    chart.draw(data, options);
+  };
+
+  InitTasks();
+
 });
-
-//.............................................................stats(overall,month,week)
-
-function AreaChartOverall() {
-  var array = JSON.parse($('#active_days_area_chart').text())
-  var hours_vaxis = JSON.parse($('#hours_vaxis').text())
-  var title = JSON.parse($('#area_chart_title').text())
-  var data = google.visualization.arrayToDataTable(array);
-  var options = {
-    title: title,
-    titleTextStyle: {color: '#B2B2B2'},
-	  hAxis: {textStyle: {color: '#B2B2B2'}},
-    vAxis: {textStyle: {color: '#B2B2B2'},
-    				minValue: 0,
-    				ticks: hours_vaxis},
-    backgroundColor: '#2e3137',
-    legend: {position: 'none'},
-    chartArea: {backgroundColor: '#43464B'},
-    colors:['#00CC00']
-  };
-  var chart = new google.visualization.AreaChart(document.getElementById('area_chart_overall'));
-  chart.draw(data, options);
-}
-
-function PieChartProgress() {
-	var array = JSON.parse($('#task_progress_pie_chart').text())
-  var title = JSON.parse($('#pie_chart_task_title').text())
-  var data = google.visualization.arrayToDataTable(array);
-  var options = {
-    title: title,
-    titleTextStyle: {color: '#B2B2B2'},
-    backgroundColor: '#2e3137',
-    legend: {textStyle: {color: '#B2B2B2'}},
-    pieSliceBorderColor: '#B2B2B2',
-    slices: {0: {color: '#008A00'}, 1: {color: '#43464B'}},
-    chartArea: {top: 70},
-    height: 350,
-  };
-  var chart = new google.visualization.PieChart(document.getElementById('pie_chart_task_progress'));
-  chart.draw(data, options);
-}
-
-function PieChartTimeDetails() {
-	var array = JSON.parse($('#time_details_pie_chart').text())
-	var slices_colors = JSON.parse($('#slices_colors').text())
-  var data = google.visualization.arrayToDataTable(array);
-  var options = {
-    title: 'Activity time in details',
-    titleTextStyle: {color: '#B2B2B2'},
-    backgroundColor: '#2e3137',
-    legend: {textStyle: {color: '#B2B2B2'}},
-    pieSliceBorderColor: '#B2B2B2',
-    slices: slices_colors,
-    chartArea: {top: 70},
-    height: 350,
-  };
-  var chart = new google.visualization.PieChart(document.getElementById('pie_chart_time_details'));
-  chart.draw(data, options);
-}
-
-google.load('visualization', '1', {packages:["corechart"], callback: init_overall_stats});
-
-function init_overall_stats() {
-  AreaChartOverall();
-  PieChartProgress();
-  PieChartTimeDetails();
-}
-
-$(window).resize(function(){
-  AreaChartOverall();
-  PieChartProgress();
-  PieChartTimeDetails();
-});
-
-$(document).ready(init_overall_stats);
-$(document).on('page:load', init_overall_stats);
--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,29 +1,37 @@
-function PieChartUserProgress() {
-	var array = JSON.parse($('#user_progress_pie_chart').text())
-  var data = google.visualization.arrayToDataTable(array);
-  var options = {
-    title: 'Time of all tasks',
-    titleTextStyle: {color: '#B2B2B2'},
-    backgroundColor: '#2e3137',
-    legend: {textStyle: {color: '#B2B2B2'}},
-    pieSliceBorderColor: '#B2B2B2',
-    slices: {0: {color: '#008A00'}, 1: {color: '#43464B'}},
-    chartArea: {top: 70},
-    height: 350,
+$(function() {
+
+  InitUsers = function() {
+
+    $(document).ready(function() {
+      if ( $("#pie_chart_user_progress").length > 0 ) {
+        InitPieChartUserProgress();
+        $(window).resize(function(){ InitPieChartUserProgress() });
+      };
+    });
+
   };
-  var chart = new google.visualization.PieChart(document.getElementById('pie_chart_user_progress'));
-  chart.draw(data, options);
-}
 
-google.load('visualization', '1', {packages:["corechart"], callback: init_user_stats});
+  InitPieChartUserProgress = function() {
+    google.load('visualization', '1', {packages:["corechart"], callback: PieChartUserProgress});
+  };
 
-function init_user_stats() {
-  PieChartUserProgress();
-}
+  PieChartUserProgress = function () {
+    var array = $("#pie_chart_user_progress").data("tasks-progress");
+    var data = google.visualization.arrayToDataTable(array);
+    var options = {
+      title: 'Time of all tasks',
+      titleTextStyle: {color: '#B2B2B2'},
+      backgroundColor: '#2e3137',
+      legend: {textStyle: {color: '#B2B2B2'}},
+      pieSliceBorderColor: '#B2B2B2',
+      slices: {0: {color: '#008A00'}, 1: {color: '#43464B'}},
+      chartArea: {top: 70},
+      height: 350,
+    };
+    var chart = new google.visualization.PieChart( $("#pie_chart_user_progress")[0] );
+    chart.draw(data, options);
+  };
 
-$(window).resize(function(){
-  PieChartUserProgress();
+  InitUsers();
+
 });
-
-$(document).ready(init_user_stats);
-$(document).on('page:change', init_user_stats);
